826297201f43be43554ad0d215b9d926383d3d13
w1kado <tunakasap2812@gmail.com>
2025-04-03 12:49:22 +0000

    first commit

---
+++ python_data_type.py
@@ -0,0 +1,24 @@
+#Text Type:	str
+#Numeric Types:	int, float, complex
+#Sequence Types:	list, tuple, range
+#Mapping Type:	dict
+#Set Types:	set, frozenset
+#Boolean Type:	bool
+#Binary Types:	bytes, bytearray, memoryview
+#None Type:	NoneType
+
+x = "Hello World"	#Type = str	
+x = 20	    #Type = int 
+x = 20.5	#Type = float	
+x = 1j		#Type = complex
+x = ["apple", "banana", "cherry"]	#Type =	 list
+x = ("apple", "banana", "cherry")	#Type =	 tuple
+x = range(6)	range	#Type =  range
+x = {"name" : "John", "age" : 36}	#Type =	dict
+x = {"apple", "banana", "cherry"}	#Type = set
+x = frozenset({"apple", "banana", "cherry"})	#Type = frozenset
+x = True		 #Type =  bool
+x = b"Hello"	 #Type =  bytes
+x = bytearray(5) #Type =  bytearray		
+x = memoryview(bytes(5))	#Type = Memory view
+x = None	#Type =  None type# w3_school
