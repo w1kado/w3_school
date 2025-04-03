6b7362962c2ef2087ea9d77ef4c67a1ccb7203a3
w1kado <tunakasap2812@gmail.com>
2025-04-03 12:49:59 +0000

    first commit

---
+++ python_comment.py
@@ -0,0 +1,11 @@
+#This is a comment
+print("TUNA")
+
+print("TUNA") #This comment usable like this 
+
+
+#print("TUNA")   This is comment so its not usable for commands
+
+#Comments should be greater than one line
+
+# w3_school
