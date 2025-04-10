973e1d76b9e0bfbe30312836de16be8549bf316e
w1kado <tunakasap2812@gmail.com>
2025-04-10 14:06:12 +0200

    Update python_casting.py

--- python_casting.py
+++ python_casting.py
@@ -1,1 +1,12 @@
-# w3_school
+x = int(2)   # x will be 2
+y = int(3.4) # y will be 2
+z = int("7") # z will be 7
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
