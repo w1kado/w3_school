152a8e2b7d71805d946be4d750bef59ed53ec021
w1kado <tunakasap2812@gmail.com>
2025-04-24 12:07:39 +0000

    Add new file

---
+++ python_lists.py
@@ -0,0 +1,24 @@
+#Creating List
+thislist1 = ["lambo" , "ferrari" , "fiat"]
+print(thislist1)
+
+#List Lenght
+thislist1 = ["lambo" , "ferrari" , "fiat"]
+print(len(thislist1))
+
+#List Data types
+list1 = ["lambo" , "ferrari" , "fiat"] #Strings
+list2 = [1 , 2 , 3 , 4 , 5 , 6] #Integers
+list3 = [True , False ] #Boolean
+
+print(list1)
+print(list2)
+print(list3)
+
+#Type 
+listoflist = ["Ford" , "Renault" "Mercedes"]
+print(type(listoflist))
+
+
+#The list() Constructor
+listoflist = list((["Ford" , "Renault" "Mercedes"]))
